ca7ab8a79bc3faf0b76387dab921453134d278c4
telday <ejw393@gmail.com>
2021-11-26 15:12:40 -0500

    Setup project for distribution

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name="data_url",
     version="1.0.0",
-    packages=find_packages(include=['data_url'])
+    author="Ellis Wright",
+    author_email="ejw393@duck.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/telday/data_url",
+    project_urls={
+        "API Documentation": "https://data-url.readthedocs.io/en/latest/#"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(include=['data_url']),
+    python_requires=">=3.5"
 )
